0b9cf2b43afbc36726579bd84daea060913f2704
Pablo Carreño <pablo.s.carreno.m@gmail.com>
2021-03-20 23:55:49 -0300

    cambiando estructura y creando vista alternativa del contador

--- src/componentes/contador/version-list/ContadorVehiculo.js
+++ src/componentes/contador/version-list/ContadorVehiculo.js
@@ -6,38 +6,41 @@ function ContadorVehiculo({ tipo_vehiculo, valor, setValue, backGroundColor }) {
 
     const [counter, setCounter] = useState(valor)
 
-    function add() {
+    function add(e) {
         let newCounter = counter + 1
         setValue(tipo_vehiculo, newCounter)
         setCounter(newCounter)
+        e.stopPropagation();
     }
 
-    function substract() {
+    function substract(e) {
         let newCounter = counter - 1
         setValue(tipo_vehiculo, newCounter)
         if (newCounter >= 0)
             setCounter(newCounter)
+        e.stopPropagation();
     }
 
-    function onChange(value) {
+    function onChange(e, value) {
         let newCounter = value
         setValue(tipo_vehiculo, newCounter)
         if (newCounter >= 0)
             setCounter(newCounter)
+        e.stopPropagation();
     }
 
     return (
         <List.Item
             key={tipo_vehiculo}
             actions={[
-                <InputNumber size="middle" style={{ width: 50 }} defaultValue={counter} value={counter} onChange={onChange}  ></InputNumber>,
-                <Button type="danger" icon={<MinusOutlined />} size="default" onClick={() => substract()} />
+                <InputNumber size="middle" style={{ width: 50 }} defaultValue={counter} value={counter} onChange={(e) => onChange(e)}  ></InputNumber>,
+                <Button type="danger" icon={<MinusOutlined />} size="default" onClick={(e) => substract(e)} />
             ]}
             onClick={() => add()}
         >
             <List.Item.Meta
                 avatar={
-                    <Button style={{ color: '#ffffff', backgroundColor: backGroundColor }} icon={<CarOutlined size="large" />} size="large" onClick={() => add()} />
+                    <Button style={{ color: '#ffffff', backgroundColor: backGroundColor }} icon={<CarOutlined size="large" />} size="large" onClick={(e) => add(e)} />
                 }
             />
             <div><h2>{tipo_vehiculo}</h2></div>
